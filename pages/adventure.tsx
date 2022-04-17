2cc951244c6ee43ee50f9e90c82e6248d66838b8
bishalpandit <bishalpandit17@gmail.com>
2022-04-17 11:46:53 +0530

    Fixed adventure screen

---
+++ pages/adventure.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function Adventure() {
+  return (
+    <div>adventure</div>
+  )
+}
+
+export default Adventure
--- pages/splash.tsx
+++ pages/splash.tsx
@@ -4,6 +4,7 @@ import Image from 'next/image'
 function Splash() {
     return (
         <div className='relative flex items-center justify-center h-screen overflow-hidden'>
+            
             <video
                 autoPlay
                 loop
@@ -16,14 +17,17 @@ function Splash() {
                 />
                 Your browser does not support the video tag.
             </video>
+
             <div className='absolute top-4 md:top-8 left-2 md:left-6'>
                 <Image src='/logo.png' height={50} width={182} alt='brand-logo' />
             </div>
+
             <div className='mid-content space-y-8 h-screen flex flex-col justify-center items-center z-20 px-6 md:px-1'>
                 <h1 className='font-poppins font-extrabold md:font-bold text-2xl md:text-5xl'>Your World of Adventures</h1>
                 <p className='font-poppins text-sm font-[450] text-justify'>Explore adventures, attend local events, go on animal safaris. Just dive in!</p>
                 <button className='font-poppins rounded-md px-3 py-4 bg-white font-medium tracking-widest text-black w-40 md:w-60'>Login</button>
             </div>
+
         </div>
     )
 }
