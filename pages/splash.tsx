38e623d9c9bb6a3a4e4b2fcb16c46ea1b7b9e09d
bishalpandit <bishalpandit17@gmail.com>
2022-04-18 19:32:06 +0530

    Fixing Asynchronous Video Fetching

--- pages/splash.tsx
+++ pages/splash.tsx
@@ -11,54 +11,50 @@ function Splash() {
     useEffect(() => {
         setTimeout(() => {
             setIsLoading(false);
-        }, 6000);
+        }, 4000);
 
         const timer = setInterval(() => {
             setProgress((prevProgress) => (prevProgress >= 100 ? 0 : prevProgress + 10));
-          }, 800);
-      
-          return () => {
+        }, 800);
+
+        return () => {
             clearInterval(timer);
-          };
+        };
     }, []);
-    
+
     return (
         <div >
             {
-                isLoading 
-                ? 
-                <div className='flex flex-col h-screen justify-center items-center'>
+                isLoading &&
+                <div className={'flex flex-col h-screen justify-center items-center '}>
                     <CircularProgress variant="determinate" thickness={4.5} className='!text-white' size={60} value={progress} />
-                </div> 
-                :
-                    (
-                        <div className='relative flex items-center justify-center h-screen overflow-hidden'>
-                            <video
-                                autoPlay
-                                loop
-                                muted
-                                className="absolute -z-10 w-auto min-w-full min-h-full max-w-none opacity-30"
-                            >
-                                <source
-                                    src="https://adventuresy-apis.azurewebsites.net/api/stream/splash"
-                                    type="video/webm"
-                                />
-                                Your browser does not support the video tag.
-                            </video>
-
-                            <div className='absolute top-4 md:top-8 left-2 md:left-6'>
-                                <Image src='/logo.png' height={50} width={182} alt='brand-logo' />
-                            </div>
-
-                            <div className='mid-content space-y-8 h-screen flex flex-col justify-center items-center z-20 px-6 md:px-1'>
-                                <h1 className='font-poppins font-extrabold md:font-bold text-2xl md:text-5xl'>Your World of Adventures</h1>
-                                <p className='font-poppins text-sm font-[450] text-justify'>Explore adventures, attend local events, go on animal safaris. Just dive in!</p>
-                                <button onClick={() => { }} className='font-poppins rounded-md px-3 py-4 bg-white font-medium tracking-widest text-black w-40 md:w-60'>Login</button>
-                            </div>
-                        </div>
-
-                    )
+                </div>
             }
+
+            <div className={`relative items-center justify-center h-screen overflow-hidden ${isLoading ? 'hidden' : 'flex'}`}>
+                <video
+                    autoPlay
+                    loop
+                    muted
+                    className="absolute -z-10 w-auto min-w-full min-h-full max-w-none opacity-30"
+                >
+                    <source
+                        src="https://adventuresy-apis.azurewebsites.net/api/stream/splash"
+                        type="video/webm"
+                    />
+                    Your browser does not support the video tag.
+                </video>
+
+                <div className='absolute top-4 md:top-8 left-2 md:left-6'>
+                    <Image src='/logo.png' height={50} width={182} alt='brand-logo' />
+                </div>
+
+                <div className='mid-content space-y-8 h-screen flex flex-col justify-center items-center z-20 px-6 md:px-1'>
+                    <h1 className='font-poppins font-extrabold md:font-bold text-2xl md:text-5xl'>Your World of Adventures</h1>
+                    <p className='font-poppins text-sm font-[450] text-justify'>Explore adventures, attend local events, go on animal safaris. Just dive in!</p>
+                    <button onClick={() => { }} className='font-poppins rounded-md px-3 py-4 bg-white font-medium tracking-widest text-black w-40 md:w-60'>Login</button>
+                </div>
+            </div>
         </div>
     )
 }
